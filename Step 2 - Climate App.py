11de919b4573f76bc284d01c857820ee24a913a4
aynewson <37918144+aynewson@users.noreply.github.com>
2019-02-20 14:16:58 -0800

    Add files via upload

---
+++ Step 2 - Climate App.py
@@ -0,0 +1,83 @@
+import datetime as dt
+import numpy as np
+import pandas as pd
+
+import sqlalchemy
+from sqlalchemy.ext.automap import automap_base
+from sqlalchemy.orm import Session
+from sqlalchemy import create_engine, func
+
+from flask import Flask, jsonify
+
+engine = create_engine("sqlite:///hawaii.sqlite")
+
+Base = automap_base()
+Base.prepare(engine, reflect=True)
+
+Measurement = Base.classes.measurmenet
+Station = Base.classes.station
+
+session = Session(engine)
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def welcome():
+	return(
+		f"Welcome to the Hawaii Climate Analysis API! <br/>"
+        f"Available Routes: <br/>"
+        f"/api/v1.0/precipitation<br/>"
+        f"/api/v1.0/stations <br/>"
+        f"/api/v1.o/tobs<br/>"
+        f"/api/v1.0/temp/start/end"
+	)
+
+
+@app.route("/api/v1.0/precipitation")
+def precipitation():
+	previous_year = dt.date(2017, 11, 19) - dt.timedeltat(days=365)
+
+    precipitation = session.query(Measurement.date, Measurement.prcp).\
+		filter(Measurement.date >= previous_year).all()
+        
+	precip = {date: prcp for date, prcp in precipitation}
+    return jsonify(precip)
+
+@app.route("/api/v1.0/stations")
+def stations()
+    results = session.query(Station.station).all()
+
+    stations = list(np.ravel(results))
+    return jsonify(stations)
+
+@app.rout("/avi/v1.0/tobs")
+def temperature_monthly
+    previous_year = dt.date(2017, 11, 19) - dt.timedelta(days=365)
+
+    results =session.query(Measurement.tobs).\
+        filter(Measurement.station == "USC00519281").\
+        filter(Measurement.date >= previous_year).all()
+    
+    temps = list(np.ravel(results))
+
+    return jsonify(temps)
+
+@app.route("/api/v1.0/temp/<start>")
+@app.route("/api/v1.0/temp/<start>/<end>")
+def stats(start=None, end=None):
+
+    sel = [func.min(Measurement.tobs), func.avg(Measurement.tobs), func.max(Measurement.tobs)]
+
+    if not end:
+        results = session.query(*sel).\
+            filter(Measurement.date >= start).\
+            filter(Measurement.date <= end).all()
+
+    temps = list(np.ravel(results))
+    return jsonify(temps)
+
+if __name__ == "__main__":
+    app.run()
+
+
